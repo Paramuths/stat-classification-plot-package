c44a0c8fea6aa6dc54a3c4d8a04acf51d02c2490
Paramuth Samuthrsindh <paramuth@mit.edu>
2023-08-21 00:29:10 +0700

    modified:   README.md 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
 from stat_plot import __version__
 
+# get the description from README.md
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+# specify the dependencies
 dependency = [
     'numpy>=1.22.4',
     'matplotlib>=3.5.3',
@@ -30,6 +36,9 @@ setup(
     author_email='paramuths@gmail.com',
 
     packages=find_packages(),
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 
     # dependency installed in general
     install_requires=[
